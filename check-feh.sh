383f472f486dc61daacb7ac87c50431f5b8932e5
StoianVassilev <stoian.vassilev@gmail.com>
2026-01-12 22:02:35 -0500

    Update check-feh.sh

--- check-feh.sh
+++ check-feh.sh
@@ -7,3 +7,5 @@ else
     DISPLAY=:0.0 XAUTHORITY=/home/stoian/.Xauthority /usr/bin/feh --draw-filename --quiet --randomize --info "echo IP:$(hostname -I)" --recursive --auto-zoom --fullscreen --hide-pointer --slideshow-delay 60.0 /home/stoian/Pictures /media/pi /boot/Pictures &
     echo "FEH started"
 fi
+
+#test change#1
